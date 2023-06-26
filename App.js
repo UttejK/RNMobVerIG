bd5e6a62c0cca2a8d241088cdd0f0c84ee4fe975
UttejK <uttej09092001@gmail.com>
2023-06-26 11:51:29 +0530

    fix 2

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { Cloudinary } from "@cloudinary/url-gen";
+import ReactNativeBlobUtil from "react-native-blob-util";
 import { StatusBar } from "expo-status-bar";
 import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import { View, useColorScheme } from "react-native";
-import axios from "axios";
-
 import {
   Button,
-  PaperProvider,
-  TextInput,
-  Switch,
   Card,
+  Provider as PaperProvider,
+  Switch,
+  Text,
+  TextInput,
 } from "react-native-paper";
 import {
   MD3LightTheme,
@@ -53,53 +53,54 @@ export default function App() {
     formData.append("file", blob);
     formData.append("upload_preset", "uploadtoCloudinary");
     let data = "";
-    await axios
-      .post(
+    try {
+      const response = await fetch(
         `https://api.cloudinary.com/v1_1/${
           cld.getConfig().cloud.cloudName
         }/image/upload`,
-        formData
-      )
-      .then((res, err) => {
-        try {
-          data = res.data["secure_url"];
-        } catch {
-          console.error(err);
+        {
+          method: "POST",
+          body: formData,
         }
-      });
-    // console.log(data);
+      );
+      const responseData = await response.json();
+      data = responseData["secure_url"];
+    } catch (error) {
+      console.error(error);
+    }
     return data;
   };
 
   const render = async () => {
     console.log("render started");
-    if (prompt === "") alert("enter a propmt please");
-    else {
-      let response = await hf
-        .textToImage({
-          model: "prompthero/openjourney-v4",
-          inputs: prompt,
-          parameters: {
-            width: 512,
-            height: 512,
-            negative_prompt:
-              "worst quality, low quality, anime, cartoon, mutilated, out of frame, extra fingers, mutated hands, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, bad face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, deformed tongue, children preteen kids jpeg artifacts, text, dark skin, nsfw, nude, naked",
-          },
-        })
-        .then(async (blob) => {
-          try {
-            const cloudinaryURL = await uploadToCloudinary(blob);
-            // console.log(cloudinaryURL);
-            return cloudinaryURL;
-          } catch (error) {
-            console.error(error);
-          }
-          // const image = new Image();
-          // image.src = URL.createObjectURL(blob);
-        });
-      setUrl(response);
-      setLoading(false);
+    if (prompt === "") {
+      alert("Enter a prompt, please.");
+      return;
     }
+
+    setLoading(true); // Set loading state to true before starting the rendering process
+
+    try {
+      const blob = await hf.textToImage({
+        model: "prompthero/openjourney-v4",
+        inputs: prompt,
+        parameters: {
+          width: 512,
+          height: 512,
+          negative_prompt:
+            "worst quality, low quality, anime, cartoon, mutilated, out of frame, extra fingers, mutated hands, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, bad face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, deformed tongue, children preteen kids jpeg artifacts, text, dark skin, nsfw, nude, naked",
+        },
+      });
+
+      const cloudinaryURL = await uploadToCloudinary(blob);
+
+      setUrl(cloudinaryURL);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false); // Set loading state to false in case of an error
+    }
+
     console.log("render ended");
   };
 
@@ -122,10 +123,22 @@ export default function App() {
         />
 
         <Card style={{ width: "90%", alignContent: "center" }}>
-          <Card.Cover
-            style={{ width: "95%", alignSelf: "center" }}
-            source={{ uri: url }}
-          />
+          {loading ? (
+            <ActivityIndicator
+              animating={true}
+              style={{ height: "30%", padding: 50 }}
+            />
+          ) : (
+            <Card.Cover
+              style={{ width: "95%", alignSelf: "center" }}
+              source={{
+                uri:
+                  url ||
+                  "https://images.pexels.com/photos/6152103/pexels-photo-6152103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+              }}
+            />
+          )}
+
           <Card.Actions style={{ display: "flex", flexDirection: "column" }}>
             <TextInput
               style={{ width: "95%", color: paperTheme.colors.primary }}
